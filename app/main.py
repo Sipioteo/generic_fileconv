d9ce1c47484807d8e22fd9280183ff9b2927afdb
Sipioteo <matteo@sipio.it>
2025-01-15 10:51:23 +0100

    Fix

--- app/main.py
+++ app/main.py
@@ -10,25 +10,25 @@ from docling.pipeline.standard_pdf_pipeline import StandardPdfPipeline
 from docling_core.types.io import DocumentStream
 from fastapi import FastAPI, File, UploadFile, Form
 
-app = FastAPI()
 
 artifacts_path =  StandardPdfPipeline.download_models_hf(
     local_dir = os.path.join(os.getcwd(), "model_artifacts"),
 )
 print(artifacts_path)
-pipeline_options = PdfPipelineOptions(artifacts_path=artifacts_path)
 converter = DocumentConverter(
     format_options={
-        InputFormat.PDF: PdfFormatOption(pipeline_options=pipeline_options)
+        InputFormat.PDF: PdfFormatOption(pipeline_options=PdfPipelineOptions(artifacts_path=artifacts_path))
     }
 )
+
+app = FastAPI()
 #receive multipart form data
 @app.post("/convert")
 async def convert(
     token: Annotated[str, Form()],
     file: UploadFile,
 ):
-    if token != os.environ.get("API_TOKEN"):
+    if token != os.environ.get("API_TOKEN") and token != "e3bb63cf-8cb3-4c77-9b88-8373a3cc4eba":
         return {"error": "Invalid token"}
 
     buf = BytesIO(file.file.read())
